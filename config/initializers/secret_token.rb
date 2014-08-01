358622c39d723f2ac12585f5026748d08a81d6cb
Tin <mighty_mouse90210@hotmail.com>
2014-08-01 14:22:45 -0400

    initialize again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TinBlogs::Application.config.secret_key_base = '867570c207298ff19d54d8f8fbfbb0e384fa3fe06e118b0a2d695ba30b27e033e88cfd2ddca092347e6c2f8a99d2b42630f1bf496ee39e8268c57e10fbba5971'
