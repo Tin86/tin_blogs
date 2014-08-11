0bfd4cfea6a69b6750739a64e744b5f6a4fef304
Tin <mighty_mouse90210@hotmail.com>
2014-08-11 12:09:59 -0400

    about me page implemented

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ TinBlogs::Application.routes.draw do
     resources :comments, :only => [:create]
   end
 
+  get '/about', :to => 'pages#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
